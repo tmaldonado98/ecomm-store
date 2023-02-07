df98e6d17fb3358c05713cb349a5fb845c2f0a2b
tomas <tmaldonadotrs@gmail.com>
2023-02-07 12:10:46 -0800

    Organized project folder structure. Added carousel to main page component. Added buttons with routes to main page component. Added modal for cart list.

--- ecomm-store/src/App.js
+++ ecomm-store/src/App.js
@@ -1,46 +1,27 @@
 import './App.css';
-import {Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 import Nav from './Nav';
-import Header from './Header';
-import MainAbout from './MainAbout';
-import FooterSection from './Footer';
-import { Routes } from 'react-router-dom';
+import Header from './app-page/Header';
+import MainAbout from './app-page/MainAbout';
+import FooterSection from './app-page/Footer';
 import { Typography } from '@material-ui/core';
-import Products from './pages/Products';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import React, {useState, useContext} from 'react';
 
-export const CartContext = React.createContext();
 
 function App() {
-
-
-  const [cartAmt, setCartAmt] = useState(0);
-
-  function increaseCart (){
-      // setCartAmt(cartAmt + 1)
-      // setCartAmt(prevAmt => prevAmt + 1)
-      console.log('test')
-  }
-
-  
-
   return (
-    <div >
-      <CartContext.Provider value={{cartAmt, setCartAmt}}>
+    <>
         <Nav />
-      </CartContext.Provider>
-      <header id="containerHeader">
-        <Header />
-      </header>
-      <section id='mainAbout'>
+      {/* <header id="containerHeader"> */}
+        <div id='header'><Header /></div >
+      {/* </header> */}
+      {/* <section id='mainAbout'> */}
         <MainAbout />
-      </section>
+      {/* </section> */}
       <footer>
         <FooterSection />
       </footer>
-    </div>
+    </>
   );
 }
 
