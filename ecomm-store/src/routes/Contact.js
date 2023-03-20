3f9248538098b333b82268a4a7650dbcb7ad6d53
Tomas Maldonado <tmaldonadotrs@gmail.com>
2023-03-19 17:04:40 -0700

    Finished contact page, made it responsive. Added video. Now need to work on function that sends the user's message to the admin's email address. Need to enforce regex on contact form, and add page transition when routing to another component.

--- ecomm-store/src/routes/Contact.js
+++ ecomm-store/src/routes/Contact.js
@@ -5,29 +5,62 @@ import './Contact.css';
 // import {Link } from 'react-router-dom';
 import { FaInstagram, FaEnvelope} from "react-icons/fa";
 import FooterSection from '../Footer';
-
+import { MDBBtn } from "mdb-react-ui-kit";
 
 export default function Contact(){
     return (
-    <div style={{backgroundColor: '#252525', height: '100vh'}}>
-        <Nav />
-        <h1>Contact, Commissions, or Get Help</h1>
+    <div style={{backgroundColor: '#252525'}}>
+      <Nav />
 
-        <ul className='h-box-ul'>
-            <li>
-              <a href='https://instagram.com/vea.collections?igshid=YmMyMTA2M2Y=' target='_blank' rel='noopener, noreferrer'>
-              <FaInstagram id='ig'/>
-              
-              </a>
-            </li>
-            <li>
-              <a href='mailto:mylovforrest@gmail.com' target='_blank' rel='noopener, noreferrer'>
-              <FaEnvelope id='email'/>  
-              
-              </a>
-            </li>
-        </ul>
-        <FooterSection/>
+      <section id="contact-page">  
+        <div id="contact-container">
+        <h2 className="contact-h">Have Questions? <br/> Contact Us</h2>
+          <ul className='h-box-ul'>
+              <li>
+                <p>Find us on Instagram</p>
+                <a href='https://instagram.com/vea.collections?igshid=YmMyMTA2M2Y=' target='_blank' rel='noopener, noreferrer'>
+                <FaInstagram id='ig'/>
+                
+                </a>
+              </li>
+              <li>
+                <p>Write us an email</p>
+                <a href='mailto:mylovforrest@gmail.com' target='_blank' rel='noopener, noreferrer'>
+                <FaEnvelope id='email'/>  
+                
+                </a>
+              </li>
+          </ul>
+
+          <section id='about-vid-container'>
+            <video id='about-video' muted autoPlay loop>
+              <source src={require("../assets/ContactVid1.mp4")}  type="video/mp4" ></source>
+            </video>
+          </section>
+
+        </div>
+        
+        <div id="commissions-container">
+          <h2 className="contact-h">Want to ask about commissions?</h2>
+          <fieldset>
+            <legend>Fill out this form!</legend>
+        
+            <label for='name'>What is your name?</label>
+            <input name="name" type='text'></input>
+        
+            <label for='email'>What is your email?</label>
+            <input name="email" type='text'></input>
+        
+            <label for='details'>Give us some more details</label>
+            <textarea placeholder="Suggestion of things to include:&#10;• Your estimated budget&#10;• Which size do you want?&#10;• Any other questions you may have, please write them here"></textarea>
+        
+        
+            <MDBBtn >Submit Message</MDBBtn>
+          </fieldset>
+        </div>
+        
+      </section>
+      <FooterSection/>
     </div>
     )
 }
